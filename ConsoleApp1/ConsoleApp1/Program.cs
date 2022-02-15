56dcdf0ce79cfeca51a92939978c380ba6525a4d
Some-Mars <marsg242002@gmail.com>
2022-02-15 13:02:04 +0300

    На самом деле вот тут сделал

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Bla-bla-bla-bla");
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = int.Parse(Console.ReadLine());
+            number *= 3;
+            number += 4;
+            number /= 2;
+            int[] massiv = new int[number];
+            for (int i = 0; i < number; i++)
+            {
+                massiv[i] = rnd.Next(-20,20);
+            }
+            Console.WriteLine("{0}", string.Join(" ", massiv));
+        }
+    }
+}
